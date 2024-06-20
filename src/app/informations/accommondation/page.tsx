68223dd24077f9d3939c51e49042a0ce28a8686a
HyunJinNo <nhj9831@naver.com>
2024-06-20 11:27:15 +0900

    장소 필터 모달창 컴포넌트 생성

--- src/app/informations/accommondation/page.tsx
+++ src/app/informations/accommondation/page.tsx
@@ -1,6 +1,6 @@
 import CategoryLinks from "@/components/informations/CategoryLinks";
-import InformationList from "@/components/informations/InformationList";
 import BannerContainer from "@/containers/common/BannerContainer";
+import InformationListContainer from "@/containers/informations/InformationListContainer";
 import TopListContainer from "@/containers/informations/TopListContainer";
 import { Metadata } from "next";
 
@@ -19,7 +19,7 @@ export default function page({ searchParams }: MyProps) {
       <BannerContainer />
       <TopListContainer category="여행" />
       <CategoryLinks category="숙박" />
-      <InformationList
+      <InformationListContainer
         category="숙박"
         subCategory={searchParams["subCategory"]!}
       />
