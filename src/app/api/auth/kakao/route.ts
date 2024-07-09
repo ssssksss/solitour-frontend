e7a6d281062af903d1a3af538d2c5b9fad5b98d1
SK\ssssk <ssssksss@naver.com>
2024-07-10 00:15:45 +0900

    CSR에서 env 값을 읽어오지 못하는 것으로 추측되어process.env.NEXT_PUBLIC 방식으로 변경

--- src/app/api/auth/kakao/route.ts
+++ src/app/api/auth/kakao/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY : process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI : process.env.KAKAO_REDIRECT_URI}`;
+  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY : process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
   return NextResponse.redirect(kakaoAuthUrl);
 }
