3c8f1fb6c92580414d89ae24c3bb7d1b381d12f3
SK\ssssk <ssssksss@naver.com>
2024-07-16 10:19:36 +0900

    사용자 정보 조회 api

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -2,15 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    const cookie = request.cookies.get("access_token");
-    if (!cookie) {
+    const access_cookie = request.cookies.get("access_token");
+    if (!access_cookie) {
+      const refresh_cookie = request.cookies.get("refresh_token");
+      if (!refresh_cookie) {
+        return new NextResponse("불필요한 요청", { status: 400 });
+      }
       return new NextResponse("Access token not found", { status: 401 });
     }
-
-    const response = await fetch(`${process.env.BACKEND_URL}/api/user/info`, {
+    // 사용자 정보 조회 API
+    const response = await fetch(`${process.env.BACKEND_URL}/api/users/info`, {
       method: "GET",
       headers: {
-        Cookie: `${cookie?.name}=${cookie?.value}`,
+        Cookie: `${access_cookie?.name}=${access_cookie?.value}`,
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
       },
@@ -22,7 +26,6 @@ export async function GET(request: NextRequest) {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
-    console.error(error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
