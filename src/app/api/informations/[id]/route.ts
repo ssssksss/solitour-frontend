a3ea32a6f8b96cbc084eb4019faf530529598590
HyunJinNo <nhj9831@naver.com>
2024-07-27 16:54:28 +0900

    Fix: 정보 삭제 후 cache 무효화 적용

--- src/app/api/informations/[id]/route.ts
+++ src/app/api/informations/[id]/route.ts
@@ -131,7 +131,7 @@ export async function DELETE(
       throw new Error("Internal Server Error");
     }
 
-    revalidatePath("/informations/list");
+    revalidatePath("/informations", "layout");
     return response;
   } catch (e) {
     return new Response(JSON.stringify({ error: "Failed to delete data." }), {
