4193bc7a5bde542a82b089278095696b851d1423
SK\ssssk <ssssksss@naver.com>
2024-07-27 18:10:02 +0900

    백엔드 배포 로그인 테스트

--- src/app/api/auth/kakao/route.ts
+++ src/app/api/auth/kakao/route.ts
@@ -1,6 +1,19 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}`;
+  const clientId = process.env.KAKAO_REST_API_KEY;
+  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL;
+
+  if (!clientId || !redirectUri) {
+    console.error("Environment variables are not set correctly");
+    return new Response("Environment variables are not set correctly", {
+      status: 500,
+    });
+  }
+
+  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+
+  console.log("Redirecting to:", kakaoAuthUrl); // URL 확인을 위한 로그
+
   return NextResponse.redirect(kakaoAuthUrl);
 }
