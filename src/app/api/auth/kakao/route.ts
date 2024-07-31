c9c810239b13fb32a70da4f653d92dcceea34974
SK\ssssk <ssssksss@naver.com>
2024-08-01 01:19:15 +0900

    카카오 로그인 할 떄 흰 배경에 아무런 작동 안하는 문제 발생에 대한 임시 조치(문제 원인을 파악 못함)

--- src/app/api/auth/kakao/route.ts
+++ src/app/api/auth/kakao/route.ts
@@ -1,19 +1,16 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
+  try {
   const clientId = process.env.KAKAO_REST_API_KEY;
   const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL;
+  const timestamp = new Date().toISOString(); // Get the current timestamp in ISO format
 
   if (!clientId || !redirectUri) {
-    console.error("Environment variables are not set correctly");
-    return new Response("Environment variables are not set correctly", {
-      status: 500,
-    });
+    return NextResponse.redirect("/auth/signin");
   }
 
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
-
-  try {
+  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&timestamp=${timestamp}`;
     return NextResponse.redirect(kakaoAuthUrl);
   } catch (error) {
     return NextResponse.redirect("/auth/signin");
