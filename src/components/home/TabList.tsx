d30bb553ee11c0228d716d69c000c64007899d7a
HyunJinNo <nhj9831@naver.com>
2024-08-01 23:42:17 +0900

    돋보기 아이콘 수정

--- src/components/home/TabList.tsx
+++ src/components/home/TabList.tsx
@@ -8,7 +8,7 @@ const TabList = () => {
         className="flex h-72 w-[29.375rem] flex-col justify-between rounded-2xl bg-gradient-to-br from-[#CBF6FF] to-[#EBE0FA] p-12 duration-300 hover:scale-105 max-[1024px]:h-48 max-[1024px]:w-[19.25rem] max-[1024px]:p-6 max-[744px]:w-full dark:opacity-65"
         href="/informations/list/parent-category/1?page=1"
       >
-        <div className="space-y-2">
+        <div className="flex flex-col gap-2">
           <h2 className="text-2xl font-bold text-black max-[1024px]:text-lg">
             여행 정보
           </h2>
@@ -31,7 +31,7 @@ const TabList = () => {
         className="flex h-72 w-[29.375rem] flex-col justify-between rounded-2xl bg-gradient-to-br from-[#E7FCE0] to-[#C3E9FF] p-12 duration-300 hover:scale-105 max-[1024px]:h-48 max-[1024px]:w-[19.25rem] max-[1024px]:p-6 max-[744px]:w-full dark:opacity-65"
         href="/gathering"
       >
-        <div className="space-y-2">
+        <div className="flex flex-col gap-2">
           <h2 className="text-2xl font-bold text-black max-[1024px]:text-lg">
             모임 정보
           </h2>
