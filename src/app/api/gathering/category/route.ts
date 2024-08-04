3b1d250c9f7c8c91a705d585812e5bbeb5667bd5
SK\ssssk <ssssksss@naver.com>
2024-08-05 00:39:01 +0900

    모집 카테고리 조회 api 연결

---
+++ src/app/api/gathering/category/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+      const response = await fetch(
+        `${process.env.BACKEND_URL}/api/categories/gathering`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
