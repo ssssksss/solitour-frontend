e6282316ae31ce5a7f3263f794627935f68e9fc6
SK\ssssk <ssssksss@naver.com>
2024-08-06 02:17:28 +0900

    모임 생성 api

---
+++ src/app/api/gathering/write/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// 요청 데이터의 타입 정의
+interface PlaceRegisterRequest {
+  searchId: string;
+  name: string;
+  xAxis: number;
+  yAxis: number;
+  address: string;
+}
+
+interface RequestData {
+  placeRegisterRequest: PlaceRegisterRequest;
+  gatheringCategoryId: number;
+  zoneCategoryNameParent: string;
+  zoneCategoryNameChild: string;
+  tagRegisterRequests: string; // JSON 문자열로서 해시태그 목록을 나타냄
+}
+
+export async function POST(request: NextRequest) {
+  const access_cookie = request.cookies.get("access_token");
+  if (!access_cookie) {
+    const refresh_cookie = request.cookies.get("refresh_token");
+    if (!refresh_cookie) {
+      // 리프레시 토큰이 없으므로 요청 중단
+      return new NextResponse("Refresh token not found", { status: 403 });
+    }
+    // 리프레시 토큰으로 재발급 받아 재요청 보내기 위한 응답
+    return new NextResponse("Refresh token not found", { status: 401 });
+  }
+  
+  try {
+    const requestData: RequestData = await request.json();
+
+    const response = await fetch(`${process.env.BACKEND_URL}/api/gatherings`, {
+      method: 'POST',
+      headers: {
+        Cookie: `${access_cookie?.name}=${access_cookie?.value}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    });
+//  TODO : 에러처리 작업 필요함
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.error || 'Unknown error occurred'}`);
+    }
+    const resultData = await response.json()
+    return NextResponse.json({ data: resultData, message: '데이터가 성공적으로 처리되었습니다.' }, { status: 200 });
+  } catch (error) {
+    console.error('요청 처리 중 오류 발생:', error);
+    return NextResponse.json({ error: '서버에서 오류가 발생했습니다.' }, { status: 500 });
+  }
+}
