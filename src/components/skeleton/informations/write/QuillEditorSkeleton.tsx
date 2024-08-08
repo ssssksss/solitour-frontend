f3c95869e7262710fe1194c1b48f9e5cabac55d8
HyunJinNo <nhj9831@naver.com>
2024-08-08 23:15:37 +0900

    Feat: 정보 Quill 에디터 skeleton 컴포넌트 구현

---
+++ src/components/skeleton/informations/write/QuillEditorSkeleton.tsx
@@ -0,0 +1,9 @@
+const QuillEditorSkeleton = () => {
+  return (
+    <div className="flex h-[17.5rem] w-full animate-pulse flex-col rounded-3xl border-[0.0625rem] border-gray3 p-6">
+      <div className="h-6 max-w-[28.5rem] bg-gray-100 dark:bg-slate-600" />
+    </div>
+  );
+};
+
+export default QuillEditorSkeleton;
