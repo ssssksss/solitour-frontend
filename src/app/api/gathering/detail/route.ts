725a1baeb6e9a88841248d313919dafb94da34e1
SK\ssssk <ssssksss@naver.com>
2024-08-09 23:27:15 +0900

    feat: 모임 상세 페이지 조회 API

---
+++ src/app/api/gathering/detail/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+    // URLSearchParams를 사용하여 쿼리 파라미터를 추출합니다.
+//   const url = new URL(request.url);
+//   const id = url.searchParams.get('id');
+//   console.log("route.ts 파일 테스트 : ",id);
+    const id = 1;
+
+  if (!id) {
+    return NextResponse.json({ error: 'ID가 제공되지 않았습니다.' }, { status: 400 });
+    }
+    
+      const access_cookie = request.cookies.get("access_token");
+  if (!access_cookie) {
+    const refresh_cookie = request.cookies.get("refresh_token");
+    if (!refresh_cookie) {
+      // 리프레시 토큰이 없으므로 요청 중단
+      return new NextResponse("Refresh token not found", { status: 403 });
+    }
+    // 리프레시 토큰으로 재발급 받아 재요청 보내기 위한 응답
+    return new NextResponse("Refresh token not found", { status: 401 });
+  }
+  
+
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/gatherings/2`, {
+    method: "GET",
+    headers: {
+      Cookie: `${access_cookie?.name}=${access_cookie?.value}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return new NextResponse(JSON.stringify(data), {
+      status: 200,
+    });
+  }
+  } catch (error) {
+    console.error('서버에서 데이터 처리 중 오류 발생:', error);
+    return NextResponse.json({ error: '서버 오류' }, { status: 500 });
+  }
+}
