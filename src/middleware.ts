14f70d70e2466b361a17ecb9a5bc3edda5b22b75
SK\ssssk <ssssksss@naver.com>
2024-08-09 21:57:15 +0900

    액세스 토큰 만료시 사라진 후 리프레시 토큰이 남아있다면 토큰 재발급 기능 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
 
-// This function can be marked "async" if using "await" inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // 사용자가 인증되었는지 확인하고, 인증되지 않은 사용자는
-  // 로그인 페이지로 리다이렉트합니다.
   if (
     pathname.startsWith("/informations/write") ||
     pathname.startsWith("/informations/edit") ||
+    pathname.startsWith("/gathering/write") ||
     pathname.startsWith("/diary")
   ) {
     const token = request.cookies.get("access_token");
+    const refresh_token = request.cookies.get("refresh_token");
 
-    // 토큰이 없는 경우 로그인 페이지로 리다이렉트합니다.
-    if (!token) {
+    if (!token && refresh_token) {
+      const backendResponse = await fetch(
+        `${process.env.BACKEND_URL}/api/auth/oauth2/token/refresh`,
+        {
+          method: "POST",
+          headers: {
+            Cookie: `${refresh_token.name}=${refresh_token.value}`,
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          cache: "no-store",
+          credentials: "include",
+        }
+      );
+
+      if (backendResponse.status === 200) {
+        const accessToken = backendResponse.headers.get("set-cookie");
+        const response = NextResponse.next();
+
+        if (accessToken) {
+          response.headers.set("set-cookie", accessToken);
+        }
+
+        return response;
+      } else {
+        return NextResponse.redirect(new URL("/auth/signin", request.url));
+      }
+    } else if (!token && !refresh_token) {
       return NextResponse.redirect(new URL("/auth/signin", request.url));
     }
   }
 
-  // 이미 로그인된 사용자가 로그인 페이지에 접속하려고 하면
-  // 메인 페이지로 리다이렉트합니다.
   if (pathname.startsWith("/auth")) {
     const token = request.cookies.get("access_token");
 
-    // 토큰이 있는 경우 메인 페이지로 리다이렉트합니다.
     if (token) {
       return NextResponse.redirect(new URL("/", request.url));
     }
@@ -33,11 +55,11 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// 미들웨어를 적용할 경로를 설정합니다.
 export const config = {
-  matchers: [
+  matcher: [
     "/informations/write",
     "/informations/edit",
+    "/gathering/write",
     "/diary/:path*",
     "/auth/:path*",
   ],
