17458a8acf0caea2e4405fede9bc7f4f1aad034f
HyunJinNo <nhj9831@naver.com>
2024-08-14 14:30:16 +0900

    Feat: 여행일기 상세 조회 날짜 표시

--- src/components/diary/detail/DiaryViewer.tsx
+++ src/components/diary/detail/DiaryViewer.tsx
@@ -32,7 +32,7 @@ const DiaryViewer = ({ data, days, currentDay, changeDay }: Props) => {
         {Array.from({ length: days }, (_, index) => index + 1).map((day) => (
           <button
             key={day}
-            className={`${day === 1 ? "text-main" : "text-gray2"} font-semibold hover:text-main`}
+            className={`${day === currentDay ? "text-main" : "text-gray2"} font-semibold hover:text-main`}
             onClick={() => changeDay(day)}
           >
             {day}
@@ -51,7 +51,12 @@ const DiaryViewer = ({ data, days, currentDay, changeDay }: Props) => {
         {data.title}
       </h1>
       <div className="mt-6 flex w-full flex-row items-center justify-between text-lg text-gray1 dark:text-slate-400">
-        <p>{new Date(data.startDate).toLocaleDateString("ko-KR")}</p>
+        <p>
+          {new Date(
+            new Date(data.startDate).getTime() +
+              (1000 * 60 * 60 * 24 * currentDay - 1),
+          ).toLocaleDateString("ko-KR")}
+        </p>
         <div className="flex flex-row items-center gap-1">
           <TiLocation className="text-main" size={"1.3rem"} />
           <p>{data.address}</p>
@@ -63,51 +68,6 @@ const DiaryViewer = ({ data, days, currentDay, changeDay }: Props) => {
           __html: data.diaryDays[currentDay - 1].content,
         }}
       />
-      {/* <div className="mt-16 flex w-full flex-col dark:text-slate-400">
-        <p>7월 나 홀로 3박 4일 제주도 여행을 다녀왔다.</p>
-        <p>
-          제주도는 언제가도 너무 좋아 자주 와봤는데 혼자 여행은 처음이라 많은
-          것을 느낀 여행이였다.
-        </p>
-        <div className="relative my-[2.375rem] aspect-[9/4] w-full">
-          <Image
-            className="rounded-2xl dark:opacity-65"
-            src="/example/diary/detail/diary-image1.svg"
-            alt="diary-image"
-            fill={true}
-            style={{ objectFit: "contain" }}
-          />
-        </div>
-        <p>
-          해안도로 에서 자전거를 탔을 때가 가장 기억이 많이 난다. 날씨도 너무
-          좋고 바다도 예뻐 혼자 2시간을 목적지 없이 자전거를 탔다. 새삼 체력이
-          좋아진 듯하다!
-        </p>
-        <div className="my-[2.375rem] flex flex-row items-center gap-5 max-[1024px]:flex-col">
-          <div className="relative h-[26.1875rem] w-[29.375rem] rounded-2xl max-[518px]:w-full">
-            <Image
-              className="rounded-2xl dark:opacity-65"
-              src="/example/diary/detail/diary-image2.svg"
-              alt="diary-image"
-              fill={true}
-              style={{ objectFit: "contain" }}
-            />
-          </div>
-          <div className="relative h-[26.1875rem] w-[29.375rem] rounded-2xl max-[518px]:w-full">
-            <Image
-              className="rounded-2xl dark:opacity-65"
-              src="/example/diary/detail/diary-image3.svg"
-              alt="diary-image"
-              fill={true}
-              style={{ objectFit: "contain" }}
-            />
-          </div>
-        </div>
-        <p>
-          그리고 솔리투어에서 추천해준 오셜록 티뮤지엄에 갔는데 생각보다 볼 것도
-          많고 가보길 잘한...
-        </p>
-      </div> */}
       <div className="mb-32 mt-6 flex w-full flex-row items-center justify-end gap-3 text-sm">
         <button className="flex flex-row items-center gap-1 hover:text-main dark:text-slate-400">
           <GoPencil />
