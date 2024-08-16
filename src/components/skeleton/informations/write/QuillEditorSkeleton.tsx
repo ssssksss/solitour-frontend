79c811e0b60234541f5033dc941a0f540e7ed5e3
HyunJinNo <nhj9831@naver.com>
2024-08-16 10:57:30 +0900

    Design: Quill 에디터 UI 통일

--- src/components/skeleton/informations/write/QuillEditorSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-const QuillEditorSkeleton = () => {
-  return (
-    <div className="flex h-[17.5rem] w-full animate-pulse flex-col rounded-3xl border-[0.0625rem] border-gray3 p-6">
-      <div className="h-6 max-w-[28.5rem] bg-gray-100 dark:bg-slate-600" />
-    </div>
-  );
-};
-
-export default QuillEditorSkeleton;
