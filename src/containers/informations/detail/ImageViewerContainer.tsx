def5cc1a6059df1bffd323ce57ee6ef87a767dec
HyunJinNo <nhj9831@naver.com>
2024-08-17 19:12:39 +0900

    Feat: 이미지 뷰어에 framer-motion 적용

--- src/containers/informations/detail/ImageViewerContainer.tsx
+++ src/containers/informations/detail/ImageViewerContainer.tsx
@@ -9,14 +9,39 @@ interface Props {
 }
 
 const ImageViewerContainer = ({ imageUrls, closeViewer }: Props) => {
-  const [index, setIndex] = useState<number>(0);
+  const [[index, direction], setIndex] = useState<number[]>([0, 0]);
   const length = useMemo(() => imageUrls.length, [imageUrls.length]);
+  const variants = useMemo(
+    () => ({
+      enter: (direction: number) => {
+        return {
+          x: direction > 0 ? 1000 : -1000,
+          opacity: 0,
+        };
+      },
+      center: {
+        zIndex: 1,
+        x: 0,
+        opacity: 1,
+      },
+      exit: (direction: number) => {
+        return {
+          zIndex: 0,
+          x: direction < 0 ? 1000 : -1000,
+          opacity: 0,
+        };
+      },
+    }),
+    [],
+  );
 
   return (
     <ImageViewer
       imageUrls={imageUrls}
       length={length}
       index={index}
+      direction={direction}
+      variants={variants}
       setIndex={setIndex}
       closeViewer={closeViewer}
     />
