2fdc0f32d35930ff0f82ebf99fa4899a90112a81
SK\ssssk <ssssksss@naver.com>
2024-08-18 22:45:34 +0900

    feat: 유저 이미지 변경 작업, API 문제로 작업 미완성

---
+++ src/components/mypage/MyPageUserImage.tsx
@@ -0,0 +1,79 @@
+import { dragAndDropProps } from "@/types/DragAndDrop";
+import Image from "next/image";
+import CropperComponent from "../common/cropper/CropperComponent";
+
+interface IMyPageUserImage {
+  dragAndDrop: dragAndDropProps;
+  imageBase64Data: string;
+  userImageUrl: string;
+  userSex: string;
+  isModalOpen: boolean;
+  closeCropModal: () => void;
+  onChangeImageUrl: (_: string) => void;
+}
+const MyPageUserImage = (props: IMyPageUserImage) => {
+  return (
+    <article
+      className={"flex items-center justify-center pb-[5.25rem] pt-[4.25rem]"}
+    >
+      <div className={"flex flex-col items-center"}>
+        <label
+          className={
+            "relative mb-[1rem] aspect-square w-[6.75rem] cursor-pointer rounded-[3rem] bg-[#F2FAF7] outline outline-[1px] outline-offset-[1px] outline-[#B8EDD9]"
+          }
+          htmlFor={"imageUpload"}
+          onDragEnter={props.dragAndDrop.onDragEnter}
+          onDragLeave={props.dragAndDrop.onDragLeave}
+          onDragOver={props.dragAndDrop.onDragOver}
+          onDrop={props.dragAndDrop.onDropOrInputEvent}
+        >
+          {props.userImageUrl ? (
+            <Image
+              src={props.userImageUrl}
+              alt={"user_image"}
+              width={108}
+              height={108}
+            />
+          ) : props.userSex == "MALE" ? (
+            <Image
+              src={"/user_sex_man_default_image.svg"}
+              alt={"user_image"}
+              width={108}
+              height={108}
+            />
+          ) : (
+            <Image
+              src={"/user_sex_woman_default_image.svg"}
+              alt={"user_image"}
+              width={108}
+              height={108}
+            />
+          )}
+          <div
+            className={
+              "absolute bottom-0 right-0 flex aspect-square w-[2.375rem] items-center justify-center rounded-[50%] bg-[#F4F4F4]"
+            }
+          >
+            <div className="relative h-[1.25rem] w-[1.25rem]">
+              <Image src={"/camera-icon.svg"} alt={"camera-icon-image"} fill />
+            </div>
+          </div>
+          <input
+            type={"file"}
+            id={"imageUpload"}
+            className="hidden"
+            onChange={(e) => props.dragAndDrop.onDropOrInputEvent(e)}
+          />
+        </label>
+      </div>
+      {props.isModalOpen && props.imageBase64Data && (
+        <CropperComponent
+          imageBase64Data={props.imageBase64Data}
+          closeCropModal={props.closeCropModal}
+          onChangeImageUrl={props.onChangeImageUrl}
+        />
+      )}
+    </article>
+  );
+};
+export default MyPageUserImage
--- src/containers/mypage/MyProfileContainer.tsx
+++ src/containers/mypage/MyProfileContainer.tsx
@@ -3,59 +3,17 @@
 import MyProfile from "@/components/mypage/MyProfile";
 import { userResponseDto } from "@/types/UserDto";
 import { fetchWithAuth } from "@/utils/fetchWithAuth";
-import { ChangeEvent, useRef, useState } from "react";
+import { useState } from "react";
 
 interface IMyProfileContainer {
   userInfo: userResponseDto;
 }
 
 const MyProfileContainer = ({userInfo}: IMyProfileContainer) => {
-  const imageUploadRef = useRef<HTMLInputElement>(null);
-  const [, setIsDragging] = useState(false);
-  const [imageUrl, setImageUrl] = useState("/");
   const [nickname, setNickname] = useState(userInfo.nickname);
   const [defaultNickname, setDefaultNickname] = useState(userInfo.nickname);
   const [message, setMessage] = useState("");
-  const onDragEnter = (e: React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setIsDragging(true);
-  };
-  const onDragLeave = (e: React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setIsDragging(false);
-  };
-  const onDragOver = (e: React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (e.dataTransfer.files) {
-      setIsDragging(true);
-    }
-  };
-  const onDrop = (e: React.DragEvent<HTMLLabelElement>) => {
-    const file = e.dataTransfer.files?.[0];
-    e.preventDefault();
-    e.stopPropagation();
-    if (!file) {
-      alert("파일이 없습니다!");
-      return;
-    }
-    const result = URL.createObjectURL(file);
-    setImageUrl(result);
-    setIsDragging(false);
-  };
-  const onChangeImageUploadInputHandler = (
-    event: ChangeEvent<HTMLInputElement>,
-  ) => {
-    const file = event.target.files?.[0];
-    if (!file) {
-      alert("파일이 없습니다!");
-      return;
-    }
-    const result = URL.createObjectURL(file);
-    setImageUrl(result);
-  };
+
   const submitChangeNicknameHandler = async () => {
     if (nickname == "" && nickname == defaultNickname) return;
     const res = await fetchWithAuth("/api/mypage/change-nickname", {
@@ -83,13 +41,6 @@ const MyProfileContainer = ({userInfo}: IMyProfileContainer) => {
 
   return (
     <MyProfile
-      onDragEnter={onDragEnter}
-      onDragLeave={onDragLeave}
-      onDragOver={onDragOver}
-      onDrop={onDrop}
-      imageUploadRef={imageUploadRef}
-      imageUrl={imageUrl}
-      onChangeImageUploadInputHandler={onChangeImageUploadInputHandler}
       userInfo={userInfo}
       submitChangeNicknameHandler={submitChangeNicknameHandler}
       nickname={nickname}
