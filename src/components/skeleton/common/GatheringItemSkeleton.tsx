6990633109af79a38e6e903fbdebcf43cdeb6fa3
SK\ssssk <ssssksss@naver.com>
2024-08-19 17:35:02 +0900

    feat: 초기화면 접속시 스켈레톤 UI 작업

--- src/components/skeleton/common/GatheringItemSkeleton.tsx
+++ src/components/skeleton/common/GatheringItemSkeleton.tsx
@@ -1,51 +1,41 @@
 const GatheringItemSkeleton = () => {
   return (
-    <div className="flex h-[19rem] w-[19rem] animate-pulse flex-col justify-between rounded-2xl p-5 outline outline-2 outline-gray3 duration-300 hover:bg-[#F2FAF7] hover:outline-main dark:bg-slate-800">
+    <div className="flex w-full animate-pulse flex-col gap-[1.25rem] rounded-2xl border-0 p-6 outline outline-2 outline-offset-[-2px] outline-gray3 max-[744px]:max-w-[27.5rem] min-[745px]:min-w-[312px]">
       <div className="flex flex-col">
         <div className="flex flex-row items-center justify-between">
-          <div className="h-8 w-12 rounded-full border-[0.0625rem] bg-gray-100 px-4 py-[0.375rem] text-xs font-semibold shadow dark:bg-slate-600" />
-          <div className="h-8 w-8 bg-gray-100 dark:bg-slate-600" />
+          <div className="h-6 w-24 rounded-full bg-gray-300"></div>
+          <div className="h-7 w-7 rounded-full bg-gray-300"></div>
         </div>
-        <div className="mb-1 mt-6 h-6 w-60 bg-gray-100 dark:bg-slate-600" />
-        <div className="h-4 w-12 bg-gray-100 dark:bg-slate-600" />
+        <div className="mt-6 h-5 w-3/4 rounded bg-gray-300"></div>
+        <div className="mt-3 h-5 w-1/2 rounded bg-gray-300"></div>
       </div>
       <div className="flex flex-col gap-5">
-        <div className="flex flex-row items-center justify-between">
-          <div className="space-y-[0.375rem] text-sm font-medium">
-            <div className="flex flex-row items-center gap-3">
-              <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-              <div className="h-4 w-20 bg-gray-100 dark:bg-slate-600" />
-            </div>
-            <div className="flex flex-row items-center gap-3">
-              <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-              <div className="h-4 w-28 bg-gray-100 dark:bg-slate-600" />
-            </div>
-            <div className="flex flex-row items-center gap-3">
-              <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-              <div className="h-4 w-12 bg-gray-100 dark:bg-slate-600" />
-            </div>
-            <div className="flex flex-row items-center gap-3">
-              <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-              <div className="h-4 w-36 bg-gray-100 dark:bg-slate-600" />
-            </div>
+        <div className="grid gap-[0.625rem] text-sm font-semibold max-[744px]:grid-cols-[auto_6.25rem] max-[432px]:grid-cols-1 min-[745px]:grid-cols-1 min-[1024px]:grid-cols-2">
+          <div className="flex flex-row items-center gap-2">
+            <div className="h-5 w-5 rounded bg-gray-300"></div>
+            <div className="h-5 w-1/2 rounded bg-gray-300"></div>
+          </div>
+          <div className="flex flex-row items-center gap-2 text-black">
+            <div className="h-5 w-5 rounded bg-gray-300"></div>
+            <div className="h-5 w-1/2 rounded bg-gray-300"></div>
+          </div>
+          <div className="flex flex-row items-center gap-2 text-black">
+            <div className="h-5 w-5 rounded bg-gray-300"></div>
+            <div className="h-5 w-1/2 rounded bg-gray-300"></div>
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <div className="h-5 w-5 rounded bg-gray-300"></div>
+            <div className="h-5 w-1/2 rounded bg-gray-300"></div>
           </div>
         </div>
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row items-center gap-1">
-            <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-            <div className="h-4 w-28 bg-gray-100 dark:bg-slate-600" />
+            <div className="h-5 w-5 rounded bg-gray-300"></div>
+            <div className="h-5 w-1/2 rounded bg-gray-300"></div>
           </div>
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row items-center gap-3">
-              <div className="flex flex-row items-center gap-1 text-gray2">
-                <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-                <div className="h-4 w-6 bg-gray-100 dark:bg-slate-600" />
-              </div>
-              <div className="flex flex-row items-center gap-1 text-gray2">
-                <div className="h-4 w-4 bg-gray-100 dark:bg-slate-600" />
-                <div className="h-4 w-6 bg-gray-100 dark:bg-slate-600" />
-              </div>
-            </div>
+          <div className="flex flex-row items-center gap-3">
+            <div className="h-6 w-6 rounded-full bg-gray-300"></div>
+            <div className="h-6 w-6 rounded-full bg-gray-300"></div>
           </div>
         </div>
       </div>
--- src/containers/gathering/GatheringCardListContainer.tsx
+++ src/containers/gathering/GatheringCardListContainer.tsx
@@ -1,40 +1,81 @@
+"use client"
+
 import GatheringCardList from "@/components/gathering/GatheringCardList";
+import GatheringItemSkeleton from "@/components/skeleton/common/GatheringItemSkeleton";
+import { Gathering, GatheringsResponse } from "@/types/GatheringDto";
 import UrlQueryStringToObject from "@/utils/UrlQueryStringToObject";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import PaginationContainer from "../common/PaginationContainer";
 
-interface IGatheringCardListContainer {
+const SkeletonGatheringList = () => {
+  return (
+    <div className="my-6 grid min-h-[20rem] w-full justify-items-center gap-x-3 gap-y-3 min-[745px]:grid-cols-2">
+      {Array.from({ length: 12 }).map((_, index) => (
+        <GatheringItemSkeleton key={index} />
+      ))}
+    </div>
+  );
+};
 
-}
-const GatheringCardListContainer = (props: IGatheringCardListContainer) => {
-    const searchParams = useSearchParams();
-    const [page, setPage] = useState(searchParams.get('page') || 0);
-    const [totalData, setTotalData] = useState(10);
-    const changeGatheringPageHandler = (id: number) => {
-        let _url = `/gathering?`;
-        let temp = UrlQueryStringToObject(window.location.href) || {};
-        if (page != 0) {
-            temp.page = id;
-        }
-        Object.entries(temp).map(i => {
-        _url += i[0]+"="+i[1]+"&"
-        })      
-        if (_url.endsWith("&")) {
-        _url = _url.slice(0, -1);
-        }
-        console.log("GatheringListContainer.tsx 파일 : ", _url);
-        window.history.pushState(null, "", _url);
+const GatheringCardListContainer = () => {
+  const searchParams = useSearchParams();
+  const [page, setPage] = useState(searchParams.get("page") || 0);
+  const [totalElements, setTotalElements] = useState(1);
+  const [gatherings, setGatherings] = useState<Gathering[]>([]);
+  const [loading, setLoading] = useState(true);
+  const changeGatheringPageHandler = (id: number) => {
+    let _url = `/gathering?`;
+    let temp = UrlQueryStringToObject(window.location.href) || {};
+    if (page != 0) {
+      temp.page = id;
     }
+    Object.entries(temp).map((i) => {
+      _url += i[0] + "=" + i[1] + "&";
+    });
+    if (_url.endsWith("&")) {
+      _url = _url.slice(0, -1);
+    }
+    window.history.pushState(null, "", _url);
+  };
 
-    useEffect(() => {
-        
-    }, [searchParams])
-    return (
-    <div>
-        <GatheringCardList data={[]} />
-        <PaginationContainer currentPage={+page} totalPages={totalData} />
-      </div>
+  useEffect(() => {
+    const temp = async () => {
+      try {
+        const queryParams = UrlQueryStringToObject(window.location.href);
+        let url = "api/gathering";
+        if (queryParams != undefined) {
+          url += "?";
+          Object.entries(queryParams).map((i) => {
+            url += i[0] + "=" + i[1] + "&";
+          });
+          url = url.slice(0, -1);
+        }
+        const response = await fetch(url);
+        const data: GatheringsResponse = await response.json();
+        setGatherings(data.content);
+        setTotalElements(data.totalElements);
+      } finally {
+        setLoading(false);
+      }
+    };
+    temp();
+  }, [searchParams]);
+
+  return (
+    <>
+      {loading ? (
+        <SkeletonGatheringList />
+      ) : (
+        <div>
+          <GatheringCardList data={gatherings} />
+          <PaginationContainer
+            currentPage={+page}
+            totalPages={Math.ceil(totalElements / 12)}
+          />
+        </div>
+      )}
+    </>
   );
 };
 export default GatheringCardListContainer
