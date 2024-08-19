50892b7f7eccea62fed66817ffb1eeb41151d246
SK\ssssk <ssssksss@naver.com>
2024-08-19 17:32:19 +0900

    fix: 모임 리스트 조회 API 연결, 필터에 맞는 파라미터 쿼리스트링으로 데이터 전송

---
+++ src/app/api/gathering/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const queryString = request.url.substring(request.url.indexOf("/api/gathering")+14);
+    const access_cookie = request.cookies.get("access_token");
+
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/api/gatherings`+queryString,
+      {
+        method: "GET",
+        headers: {
+          Cookie: `${access_cookie?.name}=${access_cookie?.value}`,
+          "Content-Type": "application/json",
+          cache: "no-store"
+        },
+      },
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+      return new NextResponse(JSON.stringify(data), {
+        status: 200,
+      });
+    }
+  } catch (error) {
+    console.error("서버에서 데이터 처리 중 오류 발생:", error);
+    return NextResponse.json({ error: "서버 오류" }, { status: 500 });
+  }
+}
