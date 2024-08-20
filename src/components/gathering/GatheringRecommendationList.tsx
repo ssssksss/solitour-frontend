41b4a3c89f96b689a73788330ef366f0c4782ac8
SK\ssssk <ssssksss@naver.com>
2024-08-20 19:36:58 +0900

    style: 모임 상세정보에서 아래 추천 모임 카드 UI 반응형 수정

--- src/components/gathering/GatheringRecommendationList.tsx
+++ src/components/gathering/GatheringRecommendationList.tsx
@@ -4,14 +4,11 @@ import GatheringItem from "../common/GatheringItem";
 const GatheringRecommendationList = ({data}: {data: GatheringRecommend[]}) => {
   // TODO
   return (
-    <div className="flex w-full my-[2.875rem] flex-col ">
-      <h2 className="text-2xl font-bold text-black">추천 여행 정보</h2>
-      <div className="my-6 grid min-[745px]:grid-cols-2 m-auto gap-x-3 gap-y-3">
+    <div className="my-[2.875rem] flex w-full flex-col">
+      <h2 className="text-2xl font-bold text-black">추천 모임 정보</h2>
+      <div className="my-6 grid min-h-[20rem] w-full justify-items-center gap-x-3 gap-y-3 min-[745px]:grid-cols-2">
         {data.map((i, index) => (
-          <GatheringItem
-            key={i.gatheringId}
-            {...i}
-          />
+          <GatheringItem key={i.gatheringId} {...i} />
         ))}
       </div>
     </div>
