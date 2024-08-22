25e65c9ea189a228bcaed9922c3ecce1787bc586
HyunJinNo <nhj9831@naver.com>
2024-08-22 17:35:07 +0900

    Feat: FeelingStatus 상수 값 추가

---
+++ src/constants/diary/feelingStatus.ts
@@ -0,0 +1,11 @@
+type FEELING_STATUS_TYPE = {
+  [feelingStatus: string]: string;
+};
+
+export const FEELING_STATUS: FEELING_STATUS_TYPE = {
+  1: "excited",
+  2: "nice",
+  3: "soso",
+  4: "sad",
+  5: "angry",
+};
