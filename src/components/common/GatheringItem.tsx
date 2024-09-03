ba9490d9f337349276aca2443cea5192351bfc55
SK\ssssk <ssssksss@naver.com>
2024-09-03 10:18:37 +0900

    style: 반응형에 따라 시계와 인원 배치 순서 변경

--- src/components/common/GatheringItem.tsx
+++ src/components/common/GatheringItem.tsx
@@ -65,85 +65,93 @@ const GatheringItem = ({ data, onBookMarkClick }: IGatheringItem) => {
           {data.userName}
         </p>
       </div>
-      <div className="flex flex-col gap-5">
-        <div className="grid gap-[0.625rem] text-sm font-semibold max-[744px]:grid-cols-[auto_7rem] max-[432px]:grid-cols-1 min-[745px]:grid-cols-1 min-[1024px]:grid-cols-2">
-          <div className="flex flex-row items-center gap-2">
-            {/* 모임 기간 */}
-            <Image
-              src={`${data.isFinish ? "/calendar-gray-icon.svg" : "/calendar-icon.svg"}`}
-              alt="calendar-icon"
-              width={14}
-              height={14}
-            />
-            {format(new Date(data.scheduleStartDate), "yyyy-MM-dd")}
-            {data.scheduleEndDate &&
-              format(new Date(data.scheduleEndDate), "~ yyyy-MM-dd")}
-          </div>
-          <div className="flex items-center gap-2 dark:text-slate-400">
-            {/* 모임 장소 */}
-            <div className={"flex h-4 min-w-[0.875rem] items-center"}>
+      <div className="flex w-full flex-col gap-5">
+        <article className="flex flex-col gap-[0.625rem]">
+          <div className="grid gap-[0.625rem] text-sm font-semibold max-[744px]:grid-cols-[auto_7rem] max-[432px]:grid-cols-1 min-[745px]:grid-cols-1 min-[1024px]:grid-cols-2">
+            <div className="flex flex-row items-center gap-2">
+              {/* 모임 기간 */}
               <Image
-                src={`${data.isFinish ? "/location-gray-icon.svg" : "/location-icon.svg"}`}
-                alt="location-icon"
+                src={`${data.isFinish ? "/calendar-gray-icon.svg" : "/calendar-icon.svg"}`}
+                alt="calendar-icon"
                 width={14}
                 height={14}
               />
+              {format(new Date(data.scheduleStartDate), "yyyy-MM-dd")}
+              {data.scheduleEndDate &&
+                format(new Date(data.scheduleEndDate), "~ yyyy-MM-dd")}
             </div>
-            <p className="overflow-hidden truncate whitespace-nowrap">
-              {data.zoneCategoryParentName} {","} {data.zoneCategoryChildName}
-            </p>
-          </div>
-          <div className="flex flex-row items-center gap-2 dark:text-slate-400">
-            <div className={"flex min-w-fit gap-2"}>
-              <div className={"relative h-5 w-[0.875rem]"}>
-                {/* 모임 인원 */}
+            <div className="flex items-center gap-2 dark:text-slate-400">
+              {/* 모임 장소 */}
+              <div className={"flex h-4 min-w-[0.875rem] items-center"}>
                 <Image
-                  src={`${data.isFinish ? "/people-gray-icon.svg" : "/people-icon.svg"}`}
-                  alt="people-icon"
+                  src={`${data.isFinish ? "/location-gray-icon.svg" : "/location-icon.svg"}`}
+                  alt="location-icon"
                   width={14}
                   height={14}
-                  className="absolute top-[50%] translate-y-[-50%]"
                 />
               </div>
-              <p
-                className={`${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
-              >
-                <span
-                  className={`${data.isFinish ? "text-gray2" : data.nowPersonCount / data.personCount > 0.5 ? "text-[#FC9F3A]" : "text-main"} ${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
-                >
-                  {data.nowPersonCount}
-                </span>
-                {"/"}
-                {data.personCount}
+              <p className="overflow-hidden truncate whitespace-nowrap">
+                {data.zoneCategoryParentName} {","} {data.zoneCategoryChildName}
               </p>
             </div>
-            <p>
-              {"(" +
-                (new Date().getFullYear() - data.startAge) +
-                "세 ~ " +
-                (new Date().getFullYear() - data.endAge) +
-                "세 ," +
-                SEX[data.allowedSex] +
-                ")"}
-            </p>
           </div>
-          <div
-            className="flex items-center gap-2"
-            style={{ lineHeight: "100%" }}
-          >
-            {/* 모임 시간 */}
-            <Image
-              src={`${data.isFinish ? "/clock-gray-icon.svg" : "/clock-icon.svg"}`}
-              alt="clock-icon"
-              width={14}
-              height={14}
-              className="ml-[.0625rem] translate-y-[1px]"
-            />
-            <span className="flex h-full items-center text-sm">
-              {format(new Date(data.scheduleStartDate), "hh:mm")}
-            </span>
+          <div className={"gap-[0.625rem] text-sm font-semibold "+ 
+                "max-[432px]:flex max-[432px]:flex-col-reverse "+
+                "min-[433px]:grid min-[433px]:grid-cols-[auto_7rem] "+
+                "min-[745px]:flex min-[745px]:flex-col-reverse min-[745px]:grid-cols-1 "+
+                "min-[1024px]:grid min-[1024px]:grid-cols-2"}>
+              <div className="flex w-full flex-row items-center gap-2 dark:text-slate-400">
+                <div className={"flex min-w-fit gap-2"}>
+                  <div className={"relative h-5 w-[0.875rem]"}>
+                    {/* 모임 인원 */}
+                    <Image
+                      src={`${data.isFinish ? "/people-gray-icon.svg" : "/people-icon.svg"}`}
+                      alt="people-icon"
+                      width={14}
+                      height={14}
+                      className="absolute top-[50%] translate-y-[-50%]"
+                    />
+                  </div>
+                  <p
+                    className={`${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
+                  >
+                    <span
+                      className={`${data.isFinish ? "text-gray2" : data.nowPersonCount / data.personCount > 0.5 ? "text-[#FC9F3A]" : "text-main"} ${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
+                    >
+                      {data.nowPersonCount}
+                    </span>
+                    {"/"}
+                    {data.personCount}
+                  </p>
+                </div>
+                <p>
+                  {"(" +
+                    (new Date().getFullYear() - data.startAge) +
+                    "세 ~ " +
+                    (new Date().getFullYear() - data.endAge) +
+                    "세 ," +
+                    SEX[data.allowedSex] +
+                    ")"}
+                </p>
+              </div>
+              <div
+                className="flex items-center gap-2"
+                style={{ lineHeight: "100%" }}
+              >
+                {/* 모임 시간 */}
+                <Image
+                  src={`${data.isFinish ? "/clock-gray-icon.svg" : "/clock-icon.svg"}`}
+                  alt="clock-icon"
+                  width={14}
+                  height={14}
+                  className="ml-[.0625rem] translate-y-[1px]"
+                />
+                <span className="flex h-full items-center text-sm">
+                  {format(new Date(data.scheduleStartDate), "hh:mm")}
+                </span>
+              </div>
           </div>
-        </div>
+        </article>
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row items-center gap-1">
             <Image
@@ -154,8 +162,8 @@ const GatheringItem = ({ data, onBookMarkClick }: IGatheringItem) => {
             />
 
             <p className="text-sm dark:text-slate-400">
-              모집마감일:
-              {format(new Date(data.deadline), "yyyy-MM-dd(EE)", {
+              마감일:
+              {format(new Date(data.deadline), "yy-MM-dd(EE)", {
                 locale: ko,
               })}
             </p>
