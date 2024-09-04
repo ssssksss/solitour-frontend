110cf8e9b6894b09f3b8a5d64d69af460cf4386e
SK\ssssk <ssssksss@naver.com>
2024-09-04 17:52:11 +0900

    feat: 모임 검색 기능 추가

--- src/containers/gathering/GatheringSearchContainer.tsx
+++ src/containers/gathering/GatheringSearchContainer.tsx
@@ -1,33 +1,71 @@
 import GatheringSearch from "@/components/gathering/GatheringSearch";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import useModalState from "@/hooks/useModalState";
+import { useSearchParams } from 'next/navigation';
+import { useState } from "react";
 
 interface IGatheringSearchContainer {
 
 }
 const GatheringSearchContainer = (props: IGatheringSearchContainer) => {
-    const keywordRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
-  
-    const searchHandler = (value: string) => {
-    const url = new URL(window.location.href);
-    const params = new URLSearchParams(url.search);
-      if (value == params.get("keyword")) return;
-      value == "" ? params.delete("keyword") : params.set("keyword", value);
+  const [searchValue, setSearchValue] = useState<string>(searchParams.get("search") || searchParams.get("tagName") ||  "");
+  const modalState = useModalState();
+  const [dropdownValue, setDropdownValue] = useState(
+    searchParams.get("tagName") != null ? "태그" : "제목",
+  );
+
+  const searchHandler = () => {
+      const url = new URL(window.location.href);
+      const params = new URLSearchParams(url.search);
+    if (searchParams.get("tagName")) {
+      // 태그 검색일 경우
+      if (searchValue == params.get("tagName")) return;
+      searchValue == ""
+        ? params.delete("tagName")
+        : params.set("tagName", searchValue);
       params.delete("page");
       url.search = params.toString();
       window.history.pushState({}, "", url.toString());
+    } else {
+      // 일반 검색일 경우
+        if (searchValue == params.get("search")) return;
+        searchValue == ""
+          ? params.delete("search")
+          : params.set("search", searchValue);
+        params.delete("page");
+        url.search = params.toString();
+        window.history.pushState({}, "", url.toString());
     }
+  }
+  
+  const dropDownHandler = (value: string) => {
+    setDropdownValue(value);
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    params.delete("page");
+    if (value == "제목") {
+      params.delete("tagName");
+      url.search = params.toString();
+      window.history.pushState({}, "", url.toString());
+      return;
+    }
+    params.delete("search");
+    // 태그 검색인 경우 글자 수 제한이 15글자이므로 글자를 제거해주는 작업
+    setSearchValue(searchValue.trim().slice(0, 15));
+    params.set("tagName", searchValue.trim().slice(0, 15) || "");
+    url.search = params.toString();
+    window.history.pushState({}, "", url.toString());
+  };
 
-    
-    useEffect(() => {
-        if (keywordRef.current) {
-            keywordRef.current.value = searchParams.get('keyword') || '';
-        }
-    },[searchParams])
-    
   return (
-      <GatheringSearch keywordRef={keywordRef} searchHandler={searchHandler} /> 
+    <GatheringSearch
+      modalState={modalState}
+      dropDownHandler={dropDownHandler}
+      dropdownValue={dropdownValue}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchHandler={searchHandler}
+    />
   );
 };
 export default GatheringSearchContainer
