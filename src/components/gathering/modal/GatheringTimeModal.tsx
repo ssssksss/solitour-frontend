ef77ecc4e952642143a344df0d0e6700aac8f5da
SK\ssssk <ssssksss@naver.com>
2024-09-04 21:09:50 +0900

    style: 버튼 및 모달 너비 간격 수정

--- src/components/gathering/modal/GatheringTimeModal.tsx
+++ src/components/gathering/modal/GatheringTimeModal.tsx
@@ -49,7 +49,7 @@ const GatheringTimeModal = (props: IGatheringTimeModalProps) => {
   return (
     <div
       className={
-        "relative h-full max-h-[24rem] w-[calc(100vw-1rem)] max-w-[25rem] overflow-y-scroll rounded-2xl bg-white px-[1rem] py-[2.875rem]"
+        "relative h-full max-h-[21.75rem] w-[calc(100vw-1rem)] max-w-[30rem] overflow-y-scroll rounded-2xl bg-white px-[2.75rem] pt-[2.75rem] pb-[2.25rem]"
       }
     >
       <button
@@ -68,60 +68,78 @@ const GatheringTimeModal = (props: IGatheringTimeModalProps) => {
         <div className={"flex w-full justify-between gap-[.5rem]"}>
           <div
             className={
-              "rounded-[1rem] px-[1rem] py-[.5rem] outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3]"
+              "rounded-[4rem] px-[1.5rem] py-[.5rem] outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3]"
             }
           >
             {format(calendarDate[0].startDate, "yyyy-MM-dd(EE)", {
               locale: ko,
             })}
           </div>
-          <select
-            name="hour"
-            className={
-              "rounded-[1rem] px-[1rem] py-[.5rem] outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3]"
-            }
-            onChange={(e) =>
-              setStartDateTime((prev) => ({
-                ...prev,
-                hour: +e.target.value,
-              }))
-            }
-            value={startDateTime.hour}
-          >
-            {Array.from({ length: 24 }, (_, i) => i).map((i) => (
-              <option value={i} key={i} defaultChecked={i == 12}>
-                {i}
-              </option>
-            ))}
-          </select>
-          <div className={"flex items-center"}> 시 </div>
-          <select
-            name="minute"
-            className={
-              "rounded-[1rem] px-[1rem] py-[.5rem] outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3]"
-            }
-            onChange={(e) =>
-              setStartDateTime((prev) => ({
-                ...prev,
-                minute: +e.target.value,
-              }))
-            }
-            value={startDateTime.minute}
-          >
-            {Array.from([...Array(6).fill(0)], (i, index) => index * 10).map(
-              (i) => (
-                <option value={i} selected={i == 0} key={i} defaultValue={0}>
+          <div className="relative">
+            <select
+              name="hour"
+              className={
+                "w-[5rem] cursor-pointer appearance-none rounded-[4rem] px-[1rem] py-[.5rem] outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3]"
+              }
+              onChange={(e) =>
+                setStartDateTime((prev) => ({
+                  ...prev,
+                  hour: +e.target.value,
+                }))
+              }
+              value={startDateTime.hour}
+            >
+              {Array.from({ length: 24 }, (_, i) => i).map((i) => (
+                <option value={i} key={i} defaultChecked={i == 12}>
                   {i}
                 </option>
-              ),
-            )}
-          </select>
+              ))}
+            </select>
+            <Image
+              src="/common/dropdown-down-arrow.svg"
+              className="absolute right-4 top-[50%]"
+              alt="location-icon"
+              width={8}
+              height={4}
+            />
+          </div>
+          <div className={"flex items-center"}> 시 </div>
+          <div className="relative">
+            <select
+              name="minute"
+              className={
+                "w-[5rem] appearance-none rounded-[4rem] px-[1rem] py-[.5rem] outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3] cursor-pointer"
+              }
+              onChange={(e) =>
+                setStartDateTime((prev) => ({
+                  ...prev,
+                  minute: +e.target.value,
+                }))
+              }
+              value={startDateTime.minute}
+            >
+              {Array.from([...Array(6).fill(0)], (i, index) => index * 10).map(
+                (i) => (
+                  <option value={i} selected={i == 0} key={i} defaultValue={0}>
+                    {i}
+                  </option>
+                ),
+              )}
+            </select>
+            <Image
+              src="/common/dropdown-down-arrow.svg"
+              className="absolute right-4 top-[50%]"
+              alt="location-icon"
+              width={8}
+              height={4}
+            />
+          </div>
           <div className={"flex items-center"}> 분 </div>
         </div>
         <div className={"flex w-full justify-center gap-[1rem] pt-[3rem]"}>
           <button
             className={
-              "h-[3rem] min-w-[8rem] rounded-[4rem] bg-main px-[1rem] py-[.5rem] text-white disabled:bg-gray1"
+              "h-[3.375rem] w-full max-w-[18.75rem] rounded-[1.75rem] bg-main px-[1rem] py-[.5rem] text-white disabled:bg-gray1"
             }
             onClick={() => submitHandler()}
           >
