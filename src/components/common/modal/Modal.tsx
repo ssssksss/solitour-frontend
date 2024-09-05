63c56339a06b27fc1900d8ca962a79cf902aff24
SK\ssssk <ssssksss@naver.com>
2024-09-05 11:46:35 +0900

    feat: 모바일에서 하단 네비게이션바에서 뒤로가기를 누를경우 모달이 닫히는게 아닌 뒤로가기 실행되는 문제가 있어서 모달창을 닫는 방식으로 변경

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -9,7 +9,6 @@ interface ModalProps extends React.PropsWithChildren {
   onClose: () => void;
 }
 
-
 export const Modal = ({ isOpen, children, onClose }: ModalProps) => {
   const [documentBody, setDocumentBody] = useState<HTMLElement | null>(null);
   const ref = useRef<HTMLDivElement>(null);
@@ -17,26 +16,48 @@ export const Modal = ({ isOpen, children, onClose }: ModalProps) => {
   useEffect(() => {
     setDocumentBody(document.body);
   }, []);
+
   useOutsideClick(ref, () => {
     onClose();
   });
+
   usePreventBodyScroll(isOpen);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      if (isOpen) {
+        onClose();
+      }
+    };
+    // 뒤로가기 버튼을 눌렀을 때 실행되는 함수
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [isOpen, onClose]);
+
+  useEffect(() => {
+    if (isOpen) {
+      history.pushState(null, "");
+    }
+  }, [isOpen]);
+
   if (!documentBody || !isOpen) return null;
 
-    return createPortal(
+  return createPortal(
+    <div
+      className="fixed inset-0 flex h-full w-full items-center justify-center"
+      style={{ zIndex: "100" }}
+    >
+      <div className="absolute h-full w-full bg-black/30"></div>
       <div
-        className="fixed inset-0 flex h-full w-full items-center justify-center"
-        style={{ zIndex: "100" }}
+        ref={ref}
+        className="flex h-full items-center justify-center pb-[1rem] pt-[1rem]"
       >
-        <div className="absolute h-full w-full bg-black/30"> </div>
-        {/* pt는 상단 네비게이션 바 높이보다 크게, pb는 바닥에서 띄우는 용도 */}
-        <div
-          ref={ref}
-          className="flex h-full items-center justify-center pb-[1rem] pt-[6rem]"
-        >
-          {children}
-        </div>
-      </div>,
-      document.getElementById("modal-root") as HTMLElement,
-    );
+        {children}
+      </div>
+    </div>,
+    document.getElementById("modal-root") as HTMLElement,
+  );
 };
