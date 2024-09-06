06575c3fb4d910256efe20db80176bbe0978cbfd
SK\ssssk <ssssksss@naver.com>
2024-09-06 15:49:37 +0900

    fix: 모임 생성시 마감일은 오늘 날짜부터 선택이 가능하게 변경하였고 모임 시작날짜보다는 이전까지만 선택가능하게 수정, 모임 시작날짜는 마감일 + 1일 부터 선택가능하게 수정

--- src/components/gathering/createUpdate/editor/modal/GatheringDeadlineModal.tsx
+++ src/components/gathering/createUpdate/editor/modal/GatheringDeadlineModal.tsx
@@ -1,5 +1,5 @@
 import "@/styles/reactDataRange.css";
-import { add, format, isValid } from "date-fns";
+import { add, compareAsc, format, isAfter, isBefore, isValid, subDays } from "date-fns";
 import { ko } from "date-fns/locale";
 import Image from "next/image";
 import { useState } from "react";
@@ -19,7 +19,7 @@ const GatheringDeadlineModal = (props: IGatheringDeadlineModalProps) => {
       ? new Date(formContext.getValues("deadline"))
       : new Date(),
   );
-
+  const scheduleStartDate = formContext.getValues("scheduleStartDate");
   const handleDateSelect = (date: Date) => {
     setDeadlineDate(date);
   };
@@ -34,18 +34,23 @@ const GatheringDeadlineModal = (props: IGatheringDeadlineModalProps) => {
 
   // maxDate를 결정하는 함수
   const getMaxDate = () => {
-    const scheduleStartDate = formContext.getValues("scheduleStartDate");
     const today = new Date();
+    // 현재날짜에서 +1년되는 날짜
     const oneYearFromToday = add(today, { years: 1 });
 
     if (scheduleStartDate) {
       const scheduleDate = new Date(scheduleStartDate);
       // 날짜가 유효한지 확인
       if (isValid(scheduleDate)) {
-        // scheduleStartDate와 오늘 날짜로부터 1년 후 중 더 이른 날짜를 반환
-        return new Date(
-          Math.min(scheduleDate.getTime(), oneYearFromToday.getTime()),
+        console.log(
+          "GatheringDeadlineModal.tsx 파일 : ",
+          compareAsc(new Date(scheduleStartDate), oneYearFromToday) < 1
+            ? new Date(scheduleStartDate)
+            : oneYearFromToday,
         );
+        return compareAsc(new Date(scheduleStartDate), oneYearFromToday) < 1
+          ? new Date(scheduleStartDate)
+          : oneYearFromToday;
       }
     }
 
@@ -56,7 +61,7 @@ const GatheringDeadlineModal = (props: IGatheringDeadlineModalProps) => {
   return (
     <div
       className={
-        "scrollbar-hide relative h-full max-h-[38rem] w-[calc(100vw-1rem)] max-w-[25rem] overflow-y-scroll rounded-2xl bg-white p-[1rem]"
+        "relative h-full max-h-[38rem] w-[calc(100vw-1rem)] max-w-[25rem] overflow-y-scroll rounded-2xl bg-white p-[1rem] scrollbar-hide"
       }
     >
       <button
@@ -70,14 +75,16 @@ const GatheringDeadlineModal = (props: IGatheringDeadlineModalProps) => {
           height={20}
         />
       </button>
-      <h2 className={"h-[2rem] text-2xl font-bold text-black mt-[2rem]"}> 모임 마감일 선택 </h2>
+      <h2 className={"mt-[2rem] h-[2rem] text-2xl font-bold text-black"}>
+        모임 마감일 선택
+      </h2>
       <section className={"flex flex-col items-center gap-[1.875rem]"}>
         <div className="relative">
           <Calendar
             date={deadlineDate}
             onChange={handleDateSelect}
             minDate={new Date()}
-            maxDate={getMaxDate()} // 동적으로 계산된 maxDate를 전달
+            maxDate={subDays(getMaxDate(), 1)} // 동적으로 계산된 maxDate를 전달
             locale={ko}
             color={"#00B488"}
             onShownDateChange={(e) => {
@@ -98,6 +105,12 @@ const GatheringDeadlineModal = (props: IGatheringDeadlineModalProps) => {
         <button
           className={`h-[3.375rem] min-w-[18.625rem] rounded-[1.75rem] bg-main px-[3.5rem] py-[1rem] text-white disabled:bg-gray1`}
           onClick={() => submitHandler()}
+          disabled={
+            !(
+              isAfter(new Date(deadlineDate), subDays(new Date(),1)) &&
+              isBefore(new Date(deadlineDate), getMaxDate())
+            )
+          }
         >
           {format(new Date(deadlineDate), "yyyy년 MM월 dd일")} 적용하기
         </button>
