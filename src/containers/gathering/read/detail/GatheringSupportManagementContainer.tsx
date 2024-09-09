8b45824a6326a80d29e36766b4e0d069d341fc74
SK\ssssk <ssssksss@naver.com>
2024-09-09 17:20:14 +0900

    style: 로그인 처리되기전에 보여줄 스켈레톤 UI 추가

--- src/containers/gathering/read/detail/GatheringSupportManagementContainer.tsx
+++ src/containers/gathering/read/detail/GatheringSupportManagementContainer.tsx
@@ -10,49 +10,58 @@ interface IGatheringSupportManagementContainer {
   isFinish: boolean;
 }
 const GatheringSupportManagementContainer = (props: IGatheringSupportManagementContainer) => {
-    const authStore = useAuthStore();
+  const authStore = useAuthStore();
   const params = useParams();
-  const [gatheringStatus, setGatheringStatus] = useState<string | null>(props.gatheringStatus)
-  const [isFinish, setIsFinish] = useState(props.isFinish);  
-    
-    // 모임 신청하기
-    const applyGathering = async () => {
-        const res = await fetch(`/api/gathering/apply?id=${params.id}`, {
-            method: "POST"
-        })
-      if (res.ok) {
-        setGatheringStatus("WAIT");
-      }
+  const [gatheringStatus, setGatheringStatus] = useState<string | null>(
+    props.gatheringStatus,
+  );
+  const [isFinish, setIsFinish] = useState(props.isFinish);
+
+  // 모임 신청하기
+  const applyGathering = async () => {
+    const res = await fetch(`/api/gathering/apply?id=${params.id}`, {
+      method: "POST",
+    });
+    if (res.ok) {
+      setGatheringStatus("WAIT");
     }
+  };
 
   // 모임 신청 취소 및 모임 신청 이후 취소, 승인 이후에도 취소 가능
-    const cancelGathering = async () => {
-        const res = await fetch(`/api/gathering/apply?id=${params.id}`, {
-          method: "DELETE",
-        });
-      if (res.ok) {
-        setGatheringStatus(null);
-      }
-  }
+  const cancelGathering = async () => {
+    const res = await fetch(`/api/gathering/apply?id=${params.id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      setGatheringStatus(null);
+    }
+  };
 
-    const reOpenGathering = async () => {
-      // 모임 다시 활성화하기
-      const response = await fetchWithAuth(
-        `/api/gathering/finish?isFinish=true&id=${params.id}`,
-        {
-          method: "PUT",
-          cache: "no-store",
-        },
-      );
+  const reOpenGathering = async () => {
+    // 모임 다시 활성화하기
+    const response = await fetchWithAuth(
+      `/api/gathering/finish?isFinish=true&id=${params.id}`,
+      {
+        method: "PUT",
+        cache: "no-store",
+      },
+    );
 
-      if (!response.ok) {
-        return;
-      }
+    if (!response.ok) {
+      return;
+    }
 
-      setIsFinish(false);
-    };
+    setIsFinish(false);
+  };
 
-  if (authStore.id < 1) return; 
+  // 로그인 작업이 처리되기전에 authStore.id == 0
+  if (authStore.id == 0)
+    return (
+      <div className="flex animate-pulse gap-2 max-[960px]:flex-col min-[960px]:flex-row">
+        <div className="h-[3.125rem] w-[7.5rem] rounded-[2.125rem] bg-gray-300" />
+        <div className="h-[3.125rem] w-[7.5rem] rounded-[2.125rem] bg-gray-300" />
+      </div>
+    );
 
   return (
     <GatheringSupportManagement
