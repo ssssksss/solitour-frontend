403555a3c3a1c4e749a0a16803227da8acc67ba9
SK\ssssk <ssssksss@naver.com>
2024-09-09 20:16:56 +0900

    style: 간격 및 아이콘 크기 변경 등 스타일 수정

--- src/components/common/GatheringItem.tsx
+++ src/components/common/GatheringItem.tsx
@@ -39,7 +39,7 @@ const GatheringItem = ({ data }: IGatheringItem) => {
   return (
     <Link
       href={`/gathering/${data.gatheringId}`}
-      className={`flex w-full ${data.isFinish ? "text-gray2" : "text-black"} flex-col gap-[1.25rem] rounded-2xl border-0 p-6 outline outline-2 outline-offset-[-2px] outline-gray3 hover:bg-[#F2FAF7] hover:outline-main max-[744px]:max-w-[27.5rem] min-[745px]:min-w-[312px] min-[1025px]:h-[16.5rem] dark:bg-slate-800 dark:outline-slate-400 dark:hover:bg-slate-600`}
+      className={`${format(new Date(data.deadline), "yyyyMMdd") < format(new Date(), "yyyyMMdd") ? "bg-[#eaeaea]" : ""} flex w-full ${data.isFinish ? "text-gray2" : "text-black"} h-auto flex-col gap-[1.25rem] rounded-2xl border-0 p-6 outline outline-2 outline-offset-[-2px] outline-gray3 hover:bg-[#F2FAF7] hover:outline-main max-[744px]:max-w-[27.5rem] min-[745px]:min-w-[312px] dark:bg-slate-800 dark:outline-slate-400 dark:hover:bg-slate-600`}
     >
       <div className="flex flex-col">
         <div className="flex flex-row items-center justify-between">
@@ -64,93 +64,104 @@ const GatheringItem = ({ data }: IGatheringItem) => {
           {data.userName}
         </p>
       </div>
-      <div className="flex w-full flex-col gap-5">
-        <article className="flex flex-col gap-[0.625rem]">
-          <div className="grid gap-[0.625rem] text-sm font-semibold max-[744px]:grid-cols-[auto_7rem] max-[432px]:grid-cols-1 min-[745px]:grid-cols-1 min-[1024px]:grid-cols-2">
-            <div className="flex flex-row items-center gap-2">
+      {/* 마감일 포함 영역 */}
+      <div className="flex w-full flex-col gap-[0.625rem]">
+        {/* 4개 영역 */}
+        <div className="flex flex-col gap-[0.625rem]">
+          <div
+            className={
+              "gap-[0.625rem] text-sm font-semibold max-[432px]:flex max-[432px]:flex-col min-[432px]:grid min-[432px]:grid-cols-[auto_7rem] min-[745px]:flex min-[745px]:grid-cols-1 min-[745px]:flex-col-reverse min-[1024px]:grid min-[1024px]:grid-cols-2"
+            }
+          >
+            <article className="flex flex-row items-center gap-2">
               {/* 모임 기간 */}
               <Image
                 src={`${data.isFinish ? "/calendar-gray-icon.svg" : "/calendar-icon.svg"}`}
                 alt="calendar-icon"
-                width={14}
-                height={14}
+                width={16}
+                height={16}
               />
-              {format(new Date(data.scheduleStartDate), "yyyy-MM-dd")}
-              {data.scheduleEndDate &&
-                format(new Date(data.scheduleEndDate), "~ yyyy-MM-dd")}
-            </div>
-            <div className="flex items-center gap-2 dark:text-slate-400">
+              {format(new Date(data.scheduleStartDate), "yyyy.MM.dd")}
+              {format(new Date(data.scheduleStartDate), "yyyyMMdd") !=
+                format(new Date(data.scheduleEndDate), "yyyyMMdd") && (
+                <span>
+                  {format(new Date(data.scheduleEndDate), "- yyyy.MM.dd")}
+                </span>
+              )}
+            </article>
+            <article className="flex h-[1.25rem] items-center gap-2 dark:text-slate-400">
               {/* 모임 장소 */}
-              <div className={"flex h-4 min-w-[0.875rem] items-center"}>
+              <div className={"flex items-center"}>
                 <Image
                   src={`${data.isFinish ? "/location-gray-icon.svg" : "/location-icon.svg"}`}
                   alt="location-icon"
-                  width={14}
-                  height={14}
+                  width={16}
+                  height={16}
                 />
               </div>
               <p className="overflow-hidden truncate whitespace-nowrap">
                 {data.zoneCategoryParentName} {","} {data.zoneCategoryChildName}
               </p>
-            </div>
+            </article>
           </div>
-          <div className={"gap-[0.625rem] text-sm font-semibold "+ 
-                "max-[432px]:flex max-[432px]:flex-col-reverse "+
-                "min-[432px]:grid min-[432px]:grid-cols-[auto_7rem] "+
-                "min-[745px]:flex min-[745px]:flex-col-reverse min-[745px]:grid-cols-1 "+
-                "min-[1024px]:grid min-[1024px]:grid-cols-2"}>
-              <div className="flex w-full flex-row items-center gap-2 dark:text-slate-400">
-                <div className={"flex min-w-fit gap-2"}>
-                  <div className={"relative h-5 w-[0.875rem]"}>
-                    {/* 모임 인원 */}
-                    <Image
-                      src={`${data.isFinish ? "/people-gray-icon.svg" : "/people-icon.svg"}`}
-                      alt="people-icon"
-                      width={14}
-                      height={14}
-                      className="absolute top-[50%] translate-y-[-50%]"
-                    />
-                  </div>
-                  <p
-                    className={`${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
-                  >
-                    <span
-                      className={`${data.isFinish ? "text-gray2" : data.nowPersonCount / data.personCount > 0.5 ? "text-[#FC9F3A]" : "text-main"} ${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
-                    >
-                      {data.nowPersonCount}
-                    </span>
-                    {"/"}
-                    {data.personCount}
-                  </p>
+          <div
+            className={
+              "gap-[0.625rem] text-sm font-semibold max-[432px]:flex max-[432px]:flex-col-reverse min-[432px]:grid min-[432px]:grid-cols-[auto_7rem] min-[745px]:flex min-[745px]:grid-cols-1 min-[745px]:flex-col-reverse min-[1024px]:grid min-[1024px]:grid-cols-2"
+            }
+          >
+            <article className="flex h-[1.25rem] w-full flex-row items-center gap-2 dark:text-slate-400">
+              <div className={"flex min-w-fit gap-2"}>
+                <div className={"relative flex items-center"}>
+                  {/* 모임 인원 */}
+                  <Image
+                    src={`${data.isFinish ? "/people-gray-icon.svg" : "/people-icon.svg"}`}
+                    alt="people-icon"
+                    width={16}
+                    height={16}
+                  />
                 </div>
-                <p>
-                  {"(" +
-                    (new Date().getFullYear() - data.startAge) +
-                    "세 ~ " +
-                    (new Date().getFullYear() - data.endAge) +
-                    "세 ," +
-                    SEX[data.allowedSex] +
-                    ")"}
+                <p
+                  className={`${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
+                >
+                  <span
+                    className={`${data.isFinish ? "text-gray2" : data.nowPersonCount / data.personCount > 0.5 ? "text-[#FC9F3A]" : "text-main"} ${data.nowPersonCount == data.personCount && "text-[#ff0000]"}`}
+                  >
+                    {data.nowPersonCount}
+                  </span>
+                  {"/"}
+                  {data.personCount}
                 </p>
               </div>
-              <div
-                className="flex items-center gap-2"
-                style={{ lineHeight: "100%" }}
-              >
-                {/* 모임 시간 */}
-                <Image
-                  src={`${data.isFinish ? "/clock-gray-icon.svg" : "/clock-icon.svg"}`}
-                  alt="clock-icon"
-                  width={14}
-                  height={14}
-                  className="ml-[.0625rem] translate-y-[1px]"
+              <p>
+                {"(" +
+                  (new Date().getFullYear() - data.startAge) +
+                  "세 ~ " +
+                  (new Date().getFullYear() - data.endAge) +
+                  "세 ," +
+                  SEX[data.allowedSex] +
+                  ")"}
+              </p>
+            </article>
+            <article
+              className="flex h-[1.25rem] items-center gap-2"
+              style={{ lineHeight: "100%" }}
+            >
+              <div className={"relative"}> 
+              {/* 모임 시간 */}
+              <Image
+                src={`${data.isFinish ? "/clock-gray-icon.svg" : "/clock-icon.svg"}`}
+                alt="clock-icon"
+                width={16}
+                height={16}
+                className="translate-y-[1px]"
                 />
-                <span className="flex h-full items-center text-sm">
-                  {format(new Date(data.scheduleStartDate), "hh:mm")}
-                </span>
-              </div>
+                </div>
+              <span className="flex h-full items-center text-sm">
+                {format(new Date(data.scheduleStartDate), "hh:mm")}
+              </span>
+            </article>
           </div>
-        </article>
+        </div>
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row items-center gap-1">
             <Image
@@ -167,10 +178,10 @@ const GatheringItem = ({ data }: IGatheringItem) => {
               })}
             </p>
           </div>
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row items-center gap-3">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-3">
               {data.isFinish ? (
-                <div className="flex flex-row items-center gap-1 text-sm">
+                <div className="flex items-center gap-1 text-sm">
                   <Image
                     src={"/common/check-gray-icon.svg"}
                     alt="check-icon"
@@ -186,12 +197,12 @@ const GatheringItem = ({ data }: IGatheringItem) => {
                   gatheringId={data.gatheringId}
                 />
               )}
-              <div className="flex flex-row items-center gap-1 text-gray2 dark:text-slate-400">
+              <div className="relative flex items-center gap-1  dark:text-slate-400">
                 <Image
                   src="/eyes-icon.svg"
                   alt="eyes-icon"
-                  width={15}
-                  height={15}
+                  width={16}
+                  height={16}
                 />
                 <p className="text-sm">
                   {convertNumberToShortForm(data.viewCount)}
