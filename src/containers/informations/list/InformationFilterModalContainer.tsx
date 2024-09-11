82a67aca9c1e1a883e0a9ede67f6142e54f8268b
HyunJinNo <nhj9831@naver.com>
2024-09-12 01:29:43 +0900

    Feat: 정보 하위 카테고리 선택 시 필터링 유지하기

--- src/containers/informations/list/InformationFilterModalContainer.tsx
+++ src/containers/informations/list/InformationFilterModalContainer.tsx
@@ -15,12 +15,13 @@ const InformationFilterModalContainer = ({ closeModal }: Props) => {
   const [place, setPlace] = useState<string | null>(searchParams.get("place"));
   const order = searchParams.get("order") ?? "latest";
   const tagName = searchParams.get("tagName");
+  const search = searchParams.get("search");
   const router = useRouter();
   const pathname = usePathname();
 
   const onClick = () => {
     router.push(
-      `${pathname}?page=1&parentCategoryId=${parentCategoryId}${childCategoryId !== null ? `&childCategoryId=${childCategoryId}` : ""}${place !== null ? `&place=${place}` : ""}&order=${order}${tagName !== null ? `&tagName=${tagName}` : ""}`,
+      `${pathname}?page=1&parentCategoryId=${parentCategoryId}${childCategoryId !== null ? `&childCategoryId=${childCategoryId}` : ""}${place !== null ? `&place=${place}` : ""}&order=${order}${tagName !== null ? `&tagName=${tagName}` : ""}${search !== null ? `&search=${search}` : ""}`,
       {
         scroll: false,
       },
@@ -31,7 +32,6 @@ const InformationFilterModalContainer = ({ closeModal }: Props) => {
   return (
     <InformationFilterModal
       place={place}
-      order={order}
       setPlace={setPlace}
       closeModal={closeModal}
       onClick={onClick}
