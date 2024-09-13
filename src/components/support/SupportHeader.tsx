a4155a3c1d48d9b55d216b831814d5ad2eedc4e9
SK\ssssk <ssssksss@naver.com>
2024-09-13 15:01:55 +0900

    fix: 상단 고객지원 UI 및 스타일 변경

--- src/components/support/SupportHeader.tsx
+++ src/components/support/SupportHeader.tsx
@@ -3,11 +3,19 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 interface ISupportHeader {
-  tabs: { name: string; value: string }[];
   active: string | null;
 }
 
-const SupportHeader = ({ tabs, active }: ISupportHeader) => {
+const tabs = [
+  { name: "서비스소개", value: "about" },
+  { name: "공지사항", value: "notice" },
+  { name: "자주 묻는 질문", value: "faq" },
+  { name: "Q&A", value: "qna" },
+  { name: "문의하기", value: "contact" },
+  { name: "약관/정책", value: "terms" },
+];
+
+const SupportHeader = ({ active }: ISupportHeader) => {
   const router = useRouter();
   const [selectedValue, setSelectedValue] = useState<string>(active || "");
 
@@ -18,40 +26,27 @@ const SupportHeader = ({ tabs, active }: ISupportHeader) => {
   };
 
   return (
-    <div className="mb-4 flex w-full flex-col items-center py-1">
-      <article className="flex w-full flex-col justify-start">
-        <h1 className="flex items-center justify-center py-8 text-3xl font-bold text-gray-800">
-          {tabs.find((i) => i.value === `${active}`)?.name || "Default Title"}
+    <div className="mb-4 flex w-full flex-col items-start py-1">
+      <div className={"flex w-full flex-col items-start gap-y-4 pt-[2.375rem]"}>
+        <h1 className={"w-full text-start text-[1.75rem] font-bold text-black"}>
+          고객지원
         </h1>
-        <div className="w-full min-[361px]:hidden">
-          <select
-            value={selectedValue}
-            onChange={handleSelectChange}
-            className="w-full rounded-md border border-gray-300 px-4 py-2 text-gray-700"
+        <p className={"text-gray1"}> 솔리투어 고객지원에서 도와드릴게요 </p>
+      </div>
+      <article className="flex justify-start gap-1 pt-12">
+        {tabs.map((tab) => (
+          <Link
+            key={tab.value}
+            href={`/support?menu=${tab.value}`}
+            className={`flex max-w-fit flex-shrink-0 transform items-center justify-center rounded-full px-6 py-3 text-lg font-medium outline-[1px] outline-offset-[-1px] outline-[#e3e3e3b8] transition-transform duration-300 ease-in-out ${
+              tab.value === `${active}`
+                ? "bg-main text-white outline-0"
+                : "text-gray-700 outline hover:bg-gray-200 hover:text-main"
+            }`}
           >
-            {tabs.map((tab) => (
-              <option key={tab.value} value={tab.value}>
-                {tab.name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <ul className="grid w-full gap-2 rounded-[1rem] px-2 py-3 outline outline-[1px] outline-offset-[-1px] outline-[#E3E3E3] max-[900px]:grid-cols-3 max-[500px]:grid-cols-2 max-[360px]:hidden min-[900px]:grid-cols-6">
-          {tabs.map((tab) => (
-            <Link
-              key={tab.value}
-              href={`/support?menu=${tab.value}`}
-              className={`flex w-full transform items-center justify-center rounded-md py-2 text-lg font-medium transition-transform duration-300 ease-in-out outline outline-[1px] outline-offset-[-1px] outline-[#e3e3e3b8] ${
-                tab.value === `${active}`
-                  ? "bg-main text-white"
-                  : "text-gray-700 hover:bg-gray-200 hover:text-main"
-              }`}
-            >
-              {tab.name}
-            </Link>
-          ))}
-        </ul>
+            {tab.name}
+          </Link>
+        ))}
       </article>
     </div>
   );
--- src/containers/support/SupportHeaderContainer.tsx
+++ src/containers/support/SupportHeaderContainer.tsx
@@ -10,18 +10,10 @@ interface ISupportHeaderContainer {
 const SupportHeaderContainer = (props: ISupportHeaderContainer) => {
 
     const searchParams = useSearchParams();
-    const tabs = [
-      { name: "서비스소개", value: "about" },
-      { name: "공지사항", value: "notice" },
-      { name: "자주 묻는 질문", value: "faq" },
-      { name: "Q&A", value: "qna" },
-      { name: "문의하기", value: "contact" },
-      { name: "약관/정책", value: "terms" },
-    ];
 
     return (
       <div>
-        <SupportHeader tabs={tabs} active={searchParams.get("menu")} />
+        <SupportHeader active={searchParams.get("menu")} />
         <SupportBodyContainer active={searchParams.get("menu")} />
       </div>
     );
