a43a4ba1ba45d9ddd52d18069a0e78d6a2aa0926
SK\ssssk <ssssksss@naver.com>
2024-09-13 10:55:53 +0900

    fix: SSR에서 CSR로 모임 상세 페이지 요청 변경

--- src/containers/gathering/read/detail/GatheringViewerContainer.tsx
+++ src/containers/gathering/read/detail/GatheringViewerContainer.tsx
@@ -1,37 +1,63 @@
 "use client"
 
 import GatheringViewer from "@/components/gathering/read/detail/GatheringViewer";
+import GatheringRecommendationList from "@/components/gathering/read/GatheringRecommendationList";
 import useModalState from "@/hooks/useModalState";
 import useGatheringStore from "@/store/gatheringStore";
 import { GatheringDetailResponseDto } from "@/types/GatheringDto";
-import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface IGatheringViewerContainer {
-  data: GatheringDetailResponseDto;
+  // data: GatheringDetailResponseDto;
   postId: number;
 }
-const GatheringViewerContainer = ({ data, postId }: IGatheringViewerContainer) => {
+const GatheringViewerContainer = ({ postId }: IGatheringViewerContainer) => {
     
   const modalState = useModalState();
   const gatheringStore = useGatheringStore();
+  const params = useParams();
+  const [data, setData] = useState<GatheringDetailResponseDto>();
 
   useEffect(() => {
-    gatheringStore.setGathering({
-      currentParticipants: data.nowPersonCount,
-      gatheringApplicantsResponses: data.gatheringApplicantsResponses,
-      isFinish: data.isFinish,
-      deadline: data.deadline,
-      personCount: data.personCount,
-    });
+
+    const getGathering = async () => {
+      const res = await fetch(`/api/gathering/${params.id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      });
+
+      const data = await res.json();
+      setData(data);
+      gatheringStore.setGathering({
+        currentParticipants: data.nowPersonCount,
+        gatheringApplicantsResponses: data.gatheringApplicantsResponses,
+        isFinish: data.isFinish,
+        deadline: data.deadline,
+        personCount: data.personCount,
+      });
+    }
+    getGathering();
   },[])
 
   return (
-    <GatheringViewer
-      data={data}
-      postId={postId}
-      modalState={modalState}
-      currentParticipants={gatheringStore.currentParticipants}
-    />
+    <>
+      {
+        data != undefined &&
+        <>
+          <GatheringViewer
+          data={data}
+          postId={postId}
+          modalState={modalState}
+          currentParticipants={gatheringStore.currentParticipants}
+          />
+          <GatheringRecommendationList data={data.gatheringRecommend} />
+      </>
+      }
+    </>
   );
 };
 export default GatheringViewerContainer
