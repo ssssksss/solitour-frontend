66e151485f2f71a8fe75ea8db2ea13b4c676677b
SK\ssssk <ssssksss@naver.com>
2024-09-14 13:10:01 +0900

    fix: font-size 14px로 수정

--- src/components/informations/common/ItemTag.tsx
+++ src/components/informations/common/ItemTag.tsx
@@ -21,7 +21,7 @@ const ItemTag = ({
 }: Props) => {
   return (
     <div
-      className={`flex flex-row items-center gap-1 text-nowrap rounded-[1rem] border-[0.0625rem] px-2 py-1 font-medium ${borderColor} ${cursorPointer ? "cursor-pointer" : ""} ${hover}`}
+      className={`flex flex-row text-sm items-center gap-1 text-nowrap rounded-[1rem] border-[0.0625rem] px-2 py-1 font-medium ${borderColor} ${cursorPointer ? "cursor-pointer" : ""} ${hover}`}
       onClick={onClick}
     >
       <p className={`${textColor}`}>#{tag}</p>
