90e03676bfefe8feb2090c86c9debffbce2d9baf
SK\ssssk <ssssksss@naver.com>
2024-09-14 11:04:36 +0900

    fix: 모임 리스트 페이지 드롭다운 가려지는 문제 수정

--- src/containers/gathering/read/GatheringSortContainer.tsx
+++ src/containers/gathering/read/GatheringSortContainer.tsx
@@ -46,22 +46,21 @@ const GatheringSortContainer = (props: IGatheringSortContainer) => {
       ></div>
     );
   return (
-    <>
-      <Dropdown
-        options={OPTIONS}
-        dropdownHandler={sortHandler}
-        value={sort}
-        defaultValue={sort}
-        dropdownContainerStyle={{
-          w: "3.5rem",
-          style: "",
-        }}
-        dropdownOptionStyle={{
-          w: "8.625rem",
-          style: "mt-[2rem] rounded-xl",
-        }}
-      />
-    </>
+    <Dropdown
+      options={OPTIONS}
+      dropdownHandler={sortHandler}
+      value={sort}
+      defaultValue={sort}
+      dropdownContainerStyle={{
+        w: "w-[3.5rem]",
+        style: "",
+      }}
+      dropdownOptionStyle={{
+        w: "w-[8rem]",
+        style: "mt-[2rem] rounded-xl",
+        transformX: "translateX(calc(3.5rem - 100%))",
+      }}
+    />
   );
 };
 export default GatheringSortContainer
