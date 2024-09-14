4ec9a27f4fa6164c4be5a3506c8435f93e4144f8
SK\ssssk <ssssksss@naver.com>
2024-09-15 00:04:12 +0900

    fix: 불필요한 코드 제거

--- src/containers/mypage/MyPageGatheringContainer.tsx
+++ src/containers/mypage/MyPageGatheringContainer.tsx
@@ -86,7 +86,6 @@ const MyPageGatheringContainer = (props: IMyPageGatheringContainer) => {
       }
 
       const data = await res.json();
-      console.log("MyPageGatheringContainer.tsx 파일 : ",data.content);
       setElements(data.content);
       setTotalElements(data.page.totalElements);
       setIsLoading(false);
