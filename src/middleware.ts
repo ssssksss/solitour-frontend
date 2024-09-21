d67bc0f0c1320f61fc188b7b16accab9c6dae9d8
HyunJinNo <nhj9831@naver.com>
2024-09-21 14:27:20 +0900

    Feat: 홈 화면에 접근할 때 미들웨어 적용

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,25 +4,23 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (
-    pathname.startsWith("/informations/write") ||
-    pathname.startsWith("/informations/edit") ||
-    pathname.startsWith("/gathering/write") ||
-    pathname.startsWith("/gathering/edit") ||
+    pathname.startsWith("/informations") ||
+    pathname.startsWith("/gathering") ||
     pathname.startsWith("/diary") ||
     pathname.startsWith("/mypage") ||
     pathname.startsWith("/support/qna/write") ||
     pathname.startsWith("/support/qna/detail")
   ) {
-    const token = request.cookies.get("access_token");
-    const refresh_token = request.cookies.get("refresh_token");
+    const accessToken = request.cookies.get("access_token");
+    const refreshToken = request.cookies.get("refresh_token");
 
-    if (!token && refresh_token) {
+    if (!accessToken && refreshToken) {
       const backendResponse = await fetch(
         `${process.env.BACKEND_URL}/api/auth/oauth2/token/refresh`,
         {
           method: "POST",
           headers: {
-            Cookie: `${refresh_token.name}=${refresh_token.value}`,
+            Cookie: `${refreshToken.name}=${refreshToken.value}`,
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
           },
@@ -38,22 +36,18 @@ export async function middleware(request: NextRequest) {
         if (accessToken) {
           response.headers.set("set-cookie", accessToken);
         }
-
         return response;
       } else {
         return NextResponse.redirect(new URL("/auth/signin", request.url));
       }
-    } else if (!token && !refresh_token) {
+    } else if (!accessToken && !refreshToken) {
       return NextResponse.redirect(new URL("/auth/signin", request.url));
     }
   }
 
-  if (pathname.startsWith("/auth")) {
-    const token = request.cookies.get("access_token");
-
-    if (token) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+  // 로그인한 상태에서 로그인 페이지에 접근하려는 경우 홈으로 이동
+  if (pathname.startsWith("/auth") && request.cookies.get("access_token")) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   if (pathname.startsWith("/support")) {
@@ -68,15 +62,43 @@ export async function middleware(request: NextRequest) {
     }
   }
 
+  // 홈 화면인 경우
+  const accessToken = request.cookies.get("access_token");
+  const refreshToken = request.cookies.get("refresh_token");
+  if (!accessToken && refreshToken) {
+    const backendResponse = await fetch(
+      `${process.env.BACKEND_URL}/api/auth/oauth2/token/refresh`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: `${refreshToken.name}=${refreshToken.value}`,
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        cache: "no-store",
+        credentials: "include",
+      },
+    );
+
+    if (backendResponse.status === 200) {
+      const accessToken = backendResponse.headers.get("set-cookie");
+      const response = NextResponse.next();
+
+      if (accessToken) {
+        response.headers.set("set-cookie", accessToken);
+      }
+      return response;
+    }
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/informations/write",
-    "/informations/edit",
-    "/gathering/write",
-    "/gathering/edit/:path*",
+    "/",
+    "/informations/:path*",
+    "/gathering/:path*",
     "/diary/:path*",
     "/auth/:path*",
     "/mypage/:path*",
