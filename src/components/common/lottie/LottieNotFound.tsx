0b50bde9173f433dccd27ba12f90e2f0843cf217
HyunJinNo <nhj9831@naver.com>
2024-09-22 23:40:36 +0900

    Design: 로티 컴포넌트 왼쪽으로 20px 이동

--- src/components/common/lottie/LottieNotFound.tsx
+++ src/components/common/lottie/LottieNotFound.tsx
@@ -6,7 +6,7 @@ import LottieComponent from "./LottieComponent";
 const LottieNotFound = ({ text }: { text: string }) => {
   return (
     <div className="relative flex aspect-square w-[20rem] items-center justify-center">
-      <LottieComponent lottieFile={LottieFile} className={"w-[8rem]"} />
+      <LottieComponent lottieFile={LottieFile} className="mr-5 w-[8rem]" />
       <p className="absolute bottom-1/2 left-1/2 flex w-full -translate-x-1/2 translate-y-[6.375rem] justify-center">
         {text}
       </p>
