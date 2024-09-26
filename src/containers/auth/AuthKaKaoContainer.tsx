bab4a8ee7303e69df4f62d565b3bf39911fc8df4
SK\ssssk <ssssksss@naver.com>
2024-09-26 20:39:24 +0900

    feat: 카카오 로그인 시 항목 동의를 받기 위해서 로직 변경

--- src/containers/auth/AuthKaKaoContainer.tsx
+++ src/containers/auth/AuthKaKaoContainer.tsx
@@ -1,23 +1,38 @@
 "use client";
 
+import AddUserInformationInitForm from "@/components/auth/AddUserInformationInitForm";
 import AuthLoading from "@/components/auth/AuthLoading";
+import { AddUserInformationFormSchema } from "@/lib/zod/schema/AddUserInformationFormSchema";
 import useAuthStore from "@/store/authStore";
-import UrlQueryStringToObject from "@/utils/UrlQueryStringToObject";
+import useToastifyStore from "@/store/toastifyStore";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 const AuthKaKaoContainer = () => {
   const router = useRouter();
   const authStore = useAuthStore();
+  const toastifyStore = useToastifyStore();
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const _queryStringObject = UrlQueryStringToObject<{
-      [key: string]: string;
-    }>(window.location.href);
+    const methods = useForm({
+      resolver: zodResolver(AddUserInformationFormSchema),
+      defaultValues: {
+        name: "",
+        age: 0,
+        sex: "",
+        isCheckTerm: false,
+        isCheckPrivacy: false,
+      },
+    });
+
     const kakaoLogin = async () => {
+    setLoading(true);
       try {
+        const url = new URL(window.location.href);
         const response = await fetch(
-          `/api/auth/kakao/getToken?code=${_queryStringObject?.code}`,
+          `/api/auth/kakao/getToken?code=${url.searchParams.get("code")}`,
           {
             method: "GET",
             headers: {
@@ -45,12 +60,79 @@ const AuthKaKaoContainer = () => {
         console.error("로그인 실패", error);
         router.push("/auth/signin");
       }
-    };
+  };
+  
+  const addUserInformationSubmit = async () => {
+          setLoading(true);
+    try {
+        const url = new URL(window.location.href);
+      const response = await fetch(
+        `/api/auth/kakao/getToken?code=${url.searchParams.get("code")}`,
+        {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          },
+          credentials: "include",
+      body: JSON.stringify({
+        sex: methods.getValues("sex"),
+        name: methods.getValues("name"),
+        age: methods.getValues("age"),
+      })
+    })
 
-    kakaoLogin();
-  }, []);
+    if (response.ok) {
+      toastifyStore.setToastify({
+        type: "success",
+        message: "등록 성공"
+      })
+      authStore.setUser({
+        sex: methods.getValues("sex"),
+        age: methods.getValues("age"),
+      });
+      }
+      
+        if (response.status !== 200) {
+          throw new Error("Failed to login");
+        }
 
-  return <AuthLoading />;
+        // 액세스 토큰을 이용해서 사용자 정보 조회
+        const userDataResponse = await fetch("/api/auth/user");
+        if (userDataResponse.status == 200) {
+          const userData = await userDataResponse.json();
+          authStore.setUser(userData);
+          router.push("/");
+        } else {
+          throw new Error("Failed to fetch user data");
+        }
+      } catch (error) {
+        console.error("로그인 실패", error);
+        router.push("/auth/signin");
+      }
+  }
+
+    // 숫자만 입력되게 필터링
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (!/^\d*$/.test(value)) return;
+    methods.setValue("age", Number(value));
+    methods.trigger();
+  };
+
+  return (
+    <>
+      {
+        loading ? <AuthLoading /> :
+        <FormProvider {...methods}>
+          <AddUserInformationInitForm
+              kakaoLogin={kakaoLogin}
+              addUserInformationSubmit={addUserInformationSubmit}
+              handleInputChange={handleInputChange}
+            />
+        </FormProvider>
+      }
+    </>
+  )
 };
 
 export default AuthKaKaoContainer;
