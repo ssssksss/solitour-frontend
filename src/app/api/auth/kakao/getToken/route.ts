c582d3f733dd7d28546af5acfb46bf2dd91898fa
SK\ssssk <ssssksss@naver.com>
2024-09-30 22:28:48 +0900

    feat: 카카오 로그인 로직 변경, 기존 유저는 바로 로그인 처리, 처음 유저는 추가적으로 정보 동의와 추가적인 정보 받게 처리

--- src/app/api/auth/kakao/getToken/route.ts
+++ src/app/api/auth/kakao/getToken/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
     const code = url.searchParams.get("code");
 
     // 백엔드에 액세스 토큰 재요청
-    const backendResponse = await fetch(
+    const response = await fetch(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/oauth2/login?type=kakao&redirectUrl=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&code=${code}`,
       {
         method: "GET",
@@ -18,17 +18,15 @@ export async function GET(request: NextRequest) {
         credentials: "include",
       },
     );
+    
 
-    const result = new NextResponse(
-      backendResponse.status == 200 ? "성공" : "실패",
-      {
-        status: backendResponse.status,
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-
-    if (backendResponse.status == 200) {
-      const cookies = backendResponse.headers.get("set-cookie");
+    const data = await response.json();
+    const result = new NextResponse(JSON.stringify(data), {
+      status: 200,
+    });
+    
+    if (response.status == 200) {
+      const cookies = response.headers.get("set-cookie");
       if (cookies) {
         // 받은 쿠키를 파싱하여 설정
         cookies.split(",").forEach((cookie) => {
@@ -51,8 +49,8 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
 
     // 백엔드에 액세스 토큰 재요청
-    const backendResponse = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/oauth2/login/kakao?redirectUrl=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&code=${code}`,
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/oauth2/login?redirectUrl=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&code=${code}`,
       {
         method: "POST",
         headers: {
@@ -69,15 +67,15 @@ export async function POST(request: NextRequest) {
     );
 
     const result = new NextResponse(
-      backendResponse.status == 200 ? "성공" : "실패",
+      response.status == 200 ? "성공" : "실패",
       {
-        status: backendResponse.status,
+        status: response.status,
         headers: { "Content-Type": "application/json" },
       },
     );
 
-    if (backendResponse.status == 200) {
-      const cookies = backendResponse.headers.get("set-cookie");
+    if (response.status == 200) {
+      const cookies = response.headers.get("set-cookie");
       if (cookies) {
         // 받은 쿠키를 파싱하여 설정
         cookies.split(",").forEach((cookie) => {
