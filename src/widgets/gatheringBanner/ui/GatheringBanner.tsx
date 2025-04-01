8f2b6b5c64c5f6c4442a381dce04c9cd8053251f
HyunJinNo <nhj9831@naver.com>
2025-04-01 20:59:18 +0900

    feat: 모임 작성 페이지로 이동하기 전 유저 정보 검증 로직 추가

--- src/widgets/gatheringBanner/ui/GatheringBanner.tsx
+++ src/widgets/gatheringBanner/ui/GatheringBanner.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import LottieImage from "@/../public/lottie/solitour_gathering_animation.json";
+import { AddUserInformationForm } from "@/features/auth";
+import { Modal } from "@/shared/ui/modal";
 import Lottie from "lottie-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useGatheringBanner } from "../model/useGatheringBanner";
 
 export const GatheringBanner = () => {
+  const { isOpen, closeModal, checkAccessGathering } = useGatheringBanner();
+
   return (
     <div className="absolute -mt-20 flex h-125 w-full flex-row items-center justify-center bg-gradient-to-br from-[#E7FCE0] to-[#C3E9FF] max-[744px]:h-144">
+      <Modal isOpen={isOpen} closeModal={closeModal}>
+        <AddUserInformationForm closeModal={closeModal} />
+      </Modal>
       <div className="flex h-96 w-240 flex-row items-center justify-between px-4 max-[1024px]:w-full max-[1024px]:px-13.5 max-[744px]:mt-16 max-[744px]:flex-col max-[744px]:justify-start">
         <div className="flex flex-col max-[744px]:items-center">
           {["<b>직접 내 모임</b>을", "<b>만들어</b>보세요!"].map(
@@ -22,6 +30,7 @@ export const GatheringBanner = () => {
           <Link
             className="mt-5 flex h-11 w-36 items-center justify-center rounded-full bg-black text-[0.9375rem] font-medium text-white shadow hover:scale-105"
             href="/gathering/write"
+            onClick={checkAccessGathering}
           >
             모임 등록하기
           </Link>
