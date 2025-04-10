1cfa2fe9f77961d1617f5b205857ba2e48bef05e
HyunJinNo <nhj9831@naver.com>
2025-04-10 19:44:30 +0900

    comment: 주석 삭제

--- src/entities/user/api/signIn.ts
+++ src/entities/user/api/signIn.ts
@@ -4,11 +4,6 @@ export async function signIn(code: string) {
     cache: "no-store",
   });
 
-  // const response = await fetch(
-  //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/oauth2/login?type=kakao&redirectUrl=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&code=${code}`,
-  //   { method: "GET", cache: "no-store" },
-  // );
-
   if (!response.ok) {
     throw new Error("Failed to sign in.");
   }
