45a251f06dd3fe1e1286e5bcc13d9a6820f5b8f2
HyunJinNo <nhj9831@naver.com>
2025-04-10 19:43:35 +0900

    fix: 미들웨어에서 쿠키를 읽지 못하는 오류 수정

--- src/entities/user/api/signIn.ts
+++ src/entities/user/api/signIn.ts
@@ -1,16 +1,17 @@
 export async function signIn(code: string) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/oauth2/login?type=kakao&redirectUrl=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&code=${code}`,
-    {
-      method: "GET",
-      credentials: "include",
-      cache: "no-store",
-    },
-  );
+  const response = await fetch(`/api/auth/kakao/getToken?code=${code}`, {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  // const response = await fetch(
+  //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/oauth2/login?type=kakao&redirectUrl=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&code=${code}`,
+  //   { method: "GET", cache: "no-store" },
+  // );
 
   if (!response.ok) {
     throw new Error("Failed to sign in.");
   }
 
-  return await response.json();
+  return response.json();
 }
